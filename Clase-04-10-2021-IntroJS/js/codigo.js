de54c5b7db35d874aadce8367250c147255cab3a
Miguel Nieto <migustanieto.2@gmail.com>
2021-10-04 21:36:08 -0500

    introduccion a JS

---
+++ Clase-04-10-2021-IntroJS/js/codigo.js
@@ -0,0 +1,152 @@
+// var nombre = "Cristian Zapata"
+// console.log(nombre)
+
+// let apellido = "mendez"
+// console.log(apellido)
+
+
+// var nombre = "Miguel"
+// console.log(nombre)
+
+
+// apellido = "nieto"
+// console.log(apellido)
+
+
+// const pi = 3.1416
+// console.log(pi)
+
+
+// var variable_textp = 'Proin eget tortor risus.'
+// console.log(variable_textp)
+
+// var variable_int = 45
+// var variable_int_2 = 3.14
+
+// console.log(variable_int)
+// console.log(variable_int_2)
+
+// var variable_bool = true
+// var variable_bool_2 = false
+
+// console.log(variable_bool)
+// console.log(variable_bool_2)
+
+// let variable_array = ["hola", 25, 3.14, variable_textp, true, false, `asdas${variable_bool}`]
+// console.log(variable_array)
+
+
+
+// var numero1 = 5
+// var numero2 = 3
+
+// console.log(numero1 - numero2)
+
+// let nombre_saludo = "Fulano perez"
+
+// console.log("Hola " + nombre_saludo)
+
+// console.log(5 * 2)
+
+// var suma1 = "cinco"
+// let suma2 = "a"
+// console.log(parseInt(suma1) + parseInt(suma2))
+// console.log(suma1 + suma2)
+
+
+// let tiene_carro = false
+
+// console.log("%cMensaje de error", "color: #fff; font-size: 3rem; background-color: #9704ec;")
+
+
+// let color = "negro"
+// var ubicacion = "alemania"
+
+// if (color == "negro") {
+//     console.log("el color es negro")
+//     if (ubicacion != "colombia") {
+//         console.warn("no es alemania")
+//     }
+
+// } else if (color == "blanco") {
+//     console.log("El color es blanco")
+// } else if (color == "azul") {
+//     console.log("El color es azul")
+// } else if (color == "amarillo") {
+//     console.log("El color es amarillo")
+// } else {
+//     console.log("No hay color conocido")
+// }
+
+
+// var numero_condicion = "5"
+
+// if (numero_condicion == 5) {
+//     console.log("el numero es igual a 5")
+// }
+
+// if (numero_condicion === 5) {
+//     console.log("el numero es igual a 5")
+// }
+
+// if (numero_condicion <= 5) {
+//     console.log("entro en la condicion zz")
+// }
+
+
+
+// var tiene_carro = true
+
+// if (!tiene_carro) {
+//     console.log("comprate carro")
+// }
+
+// if (tiene_carro) {
+//     console.log("tiene carro")
+// }
+
+
+// // and y el or   && ||
+
+// var simpsons = "familia fox"
+
+// var futurama = "familia fox"
+
+// let padre_familia = "familia disney"
+
+// if (simpsons == 'familia fox' && futurama == "familia fox" || padre_familia == "familia disney") {
+//     console.log("son de la familia fox")
+// }
+
+
+// var calamar = "asdasd"
+
+// switch (calamar) {
+//     case "rojo":
+//         console.log("muere si se mueve")
+//         break
+//     case "verde":
+//         console.log("Se puede mover")
+//         break
+//     default:
+//         console.error("directiva invalida")
+// }
+
+
+var contador = 1
+while (contador <= 10) {
+    console.log(contador)
+    contador++
+}
+
+
+var contador2 = 101
+do {
+    console.warn(contador2)
+    contador2++
+} while (contador2 <= 10);
+
+
+for (let h = 0; h <= 10; h++) {
+    console.error(h)
+}
