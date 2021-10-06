a42416b95727232797d50b23c544b6abda5d45f2
Miguel Nieto <migustanieto.2@gmail.com>
2021-10-05 21:00:02 -0500

    Update main.js

--- Clase-05-10-2021-JS-dom/js/main.js
+++ Clase-05-10-2021-JS-dom/js/main.js
@@ -23,35 +23,35 @@
 
 
 
-// var nombre = prompt("Cual es su nombre?")
-// var numero = prompt("Hasta que numero deseas la lista?")
-// let contenido = ''
+var nombre = prompt("Cual es su nombre?")
+var numero = prompt("Hasta que numero deseas la lista?")
+let contenido = ''
 
-// let titulo_web = document.querySelector("#nombre_usuario")
-// titulo_web.innerHTML = `hola ${nombre}`
+let titulo_web = document.querySelector("#nombre_usuario")
+titulo_web.innerHTML = `hola ${nombre}`
 
-// for (let x = 0; x <= numero; x++) {
-//     contenido += `<li>El numero es ${x}</li>`
-// }
+for (let x = 0; x <= numero; x++) {
+    contenido += `<li>El numero es ${x}</li>`
+}
 
-// let lista_web = document.getElementById("lista")
-// lista_web.innerHTML = `
-//     <ul>
-//         ${contenido}
-//     </ul>
-// `
+let lista_web = document.getElementById("lista")
+lista_web.innerHTML = `
+    <ul>
+        ${contenido}
+    </ul>
+`
 
 //Ejercicio 1 Taller
 // let numero_ej1 = prompt("Hasta que numero desea sumar?")
 // suma = 0
 // mensaje = ''
-// for (let x = 1; x <= numero_ej1; x++) {
+for (let x = 1; x <= numero_ej1; x++) {
 
-//     mensaje = `La operación de ${x} + ${suma} es: `
-//     suma = suma + x;
-//     mensaje = mensaje + suma
-//     console.log(mensaje);
-// }
+    mensaje = `La operación de ${x} + ${suma} es: `
+    suma = suma + x;
+    mensaje = mensaje + suma
+    console.log(mensaje);
+}
 
 // for (let x=1; x<= numero_ej1; x++){
 
@@ -72,15 +72,3 @@
 //     suma = suma + x;
 
 // }
-
-
-let tabla_multiplicar = prompt("cual tabla de multiplicar desea?")
-let contador = 0
-let mult = 0
-let operacion = 0
-
-for (let x = 1; x <= 10; x++) {
-    mult = tabla_multiplicar * x
-    console.log(`la operacion es ${tabla_multiplicar} + ${x} = ${mult}`)
-
-}
