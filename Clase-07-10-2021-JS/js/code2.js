40bf67bff8ec30638f0f9bc08596a906bb756319
Miguel Nieto <migustanieto.2@gmail.com>
2021-10-08 20:00:04 -0500

    Crud parte 2

--- Clase-07-10-2021-JS/js/code2.js
+++ Clase-07-10-2021-JS/js/code2.js
@@ -1,4 +1,5 @@
 function guardar_data() {
+    // debugger
     let contador_data = 1
 
     if (localStorage.getItem("contador") == null) {
@@ -25,7 +26,11 @@ function guardar_data() {
 
 
 function listado_data(contador_actual = 1, actualiza_tabla = false) {
+    // debugger
     let body_tabla = document.querySelector("#data-usuario-read")
+
+    let titulo_formulario = document.querySelector("#titulo_accion_formulario")
+    titulo_formulario.innerHTML = `Insertar nuevo dato`
 
     if (actualiza_tabla) {
         let contador_futuro = localStorage.getItem('contador')
@@ -35,22 +40,53 @@ function listado_data(contador_actual = 1, actualiza_tabla = false) {
                 <td>${localStorage.getItem('nombre_' + x)}</td>
                 <td>${localStorage.getItem('telefono_' + x)}</td>
                 <td>${localStorage.getItem('direccion_' + x)}</td>
-                <td></td>
+                <td>
+                    <i class="fas fa-pen mx-2" onclick="editar_elemento(${x})"></i>
+                    <i class="fas fa-trash mx-2" onclick="borrar_elemento()"></i>
+                </td>
             </tr>
             `
         }
-    }else{
+    } else {
         body_tabla.innerHTML += `
         <tr>
             <td>${localStorage.getItem('nombre_' + contador_actual)}</td>
             <td>${localStorage.getItem('telefono_' + contador_actual)}</td>
             <td>${localStorage.getItem('direccion_' + contador_actual)}</td>
-            <td></td>
+            <td>
+                <i class="fas fa-pen mx-2" onclick="editar_elemento(${contador_actual})"></i>
+                <i class="fas fa-trash mx-2" onclick="borrar_elemento()"></i>
+            </td>
         </tr>
         `
     }
 
-    
+
 }
 
 listado_data(1, true)
+
+
+function editar_elemento(indice_dato) {
+    let titulo_formulario = document.querySelector("#titulo_accion_formulario")
+    titulo_formulario.innerHTML = `Actualizar dato`
+    var input_nombre = document.getElementById('nombre_user')
+    var input_telefono = document.getElementById('telefono_user')
+    var input_direccion = document.getElementById('direccion_user')
+
+    input_nombre.value = localStorage.getItem("nombre_" + indice_dato)
+    input_telefono.value = localStorage.getItem("telefono_" + indice_dato)
+    input_direccion.value = localStorage.getItem("direccion_" + indice_dato)
+
+    console.log(input_nombre)
+    console.log(input_telefono)
+    console.log(input_direccion)
+}
+
+function editar_elemento_accion(indice_dato) {
+
+}
+
+function borrar_elemento() {
+    console.error("Se borro")
+}
