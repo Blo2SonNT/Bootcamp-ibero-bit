62cbc4022172b9109841a1ffbee8b803ae7bcb6b
Miguel Nieto <migustanieto.2@gmail.com>
2021-10-12 19:50:23 -0500

    funcion borrar - CRUD completo localstorage

--- Clase-07-10-2021-JS/js/code2.js
+++ Clase-07-10-2021-JS/js/code2.js
@@ -25,6 +25,7 @@ function guardar_data() {
 
     let contador_actual = parseInt(contador_data) - 1
     listado_data(contador_actual)
+    document.getElementById("mi_formulario").reset()
 
 }
 
@@ -38,18 +39,20 @@ function listado_data(contador_actual = 1, actualiza_tabla = false) {
 
     if (actualiza_tabla) {
         let contador_futuro = localStorage.getItem('contador')
-        for (let x = 1; x < contador_futuro; x++) {
-            body_tabla.innerHTML += `
-            <tr>
-                <td>${localStorage.getItem('nombre_' + x)}</td>
-                <td>${localStorage.getItem('telefono_' + x)}</td>
-                <td>${localStorage.getItem('direccion_' + x)}</td>
-                <td>
-                    <i class="fas fa-pen mx-2" onclick="editar_elemento(${x})"></i>
-                    <i class="fas fa-trash mx-2" onclick="borrar_elemento()"></i>
-                </td>
-            </tr>
-            `
+        for (let id_dato = 1; id_dato < contador_futuro; id_dato++) {
+            if (localStorage.getItem('nombre_' + id_dato) != null && localStorage.getItem('telefono_' + id_dato) != null && localStorage.getItem('direccion_' + id_dato)) {
+                body_tabla.innerHTML += `
+                <tr>
+                    <td>${localStorage.getItem('nombre_' + id_dato)}</td>
+                    <td>${localStorage.getItem('telefono_' + id_dato)}</td>
+                    <td>${localStorage.getItem('direccion_' + id_dato)}</td>
+                    <td>
+                        <i class="fas fa-pen mx-2" onclick="editar_elemento(${id_dato})"></i>
+                        <i class="fas fa-trash mx-2" onclick="borrar_elemento(${id_dato})"></i>
+                    </td>
+                </tr>
+                `
+            }
         }
     } else {
         body_tabla.innerHTML += `
@@ -59,7 +62,7 @@ function listado_data(contador_actual = 1, actualiza_tabla = false) {
             <td>${localStorage.getItem('direccion_' + contador_actual)}</td>
             <td>
                 <i class="fas fa-pen mx-2" onclick="editar_elemento(${contador_actual})"></i>
-                <i class="fas fa-trash mx-2" onclick="borrar_elemento()"></i>
+                <i class="fas fa-trash mx-2" onclick="borrar_elemento(${contador_actual})"></i>
             </td>
         </tr>
         `
@@ -120,6 +123,31 @@ function editar_elemento_accion(indice_dato) {
     document.getElementById("mi_formulario").reset()
 }
 
-function borrar_elemento() {
-    console.error("Se borro")
+function borrar_elemento(id_dato) {
+    Swal.fire({
+        title: 'Esta seguro de eliminar el dato?',
+        icon: 'question',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si, borrar',
+        allowOutsideClick: false
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let body_tabla = document.querySelector("#data-usuario-read")
+            body_tabla.innerHTML = ''
+
+            localStorage.removeItem("nombre_" + id_dato)
+            localStorage.removeItem("telefono_" + id_dato)
+            localStorage.removeItem("direccion_" + id_dato)
+            listado_data(1, true)
+
+
+            Swal.fire({
+                title: 'Se elimino el dato',
+                icon: 'success'
+            })
+            document.getElementById("mi_formulario").reset()
+        }
+    })
 }
