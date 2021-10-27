869b80dfbe101dee7002318b2a1248136ce05cef
Miguel Nieto <migustanieto.2@gmail.com>
2021-10-26 21:24:04 -0500

    POO JS

---
+++ Clase-26-10-2021-json-objs-js/js/code.js
@@ -0,0 +1,52 @@
+// let persona = ["julian", 25, true, ["asda"]]
+
+// let persona = {
+//     nombre: "miguel",
+//     edad: 25,
+//     correo: "correo@correo.com",
+//     tiene_hijos: true,
+//     hijos: {
+//         nombre: "junior",
+//         edad: 10,
+//         comidas_favoritas: ["pizza", "brocoli", "pasta"]
+//     }
+// }
+
+
+// console.log(persona)
+
+// console.log(Object.values(persona))
+
+// input = document.getElementById('ejemplo').value
+
+
+
+class Saludo {
+
+    nombre_saludo
+    edad_saludo = 0
+
+    constructor(nombre, edad) {
+        this.nombre_saludo = nombre;
+        this.edad_saludo = edad;
+    }
+
+    saludo_persona() {
+        return `Hola ${this.nombre_saludo}`
+    }
+
+    chao_persona() {
+        return `Adios ${this.nombre_saludo}`
+    }
+
+    edad_persona() {
+        return `La edad de la persona es ${this.edad_saludo}`
+    }
+
+
+}
+
+let edad = 40
+
+let saludando = new Saludo("Juan", edad)
+console.log(saludando.edad_persona() + " " + saludando.chao_persona())
