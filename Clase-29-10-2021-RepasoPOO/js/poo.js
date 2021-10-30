28c0785477cdd2fbefc2a371354276a9b676f54c
Miguel Nieto <migustanieto.2@gmail.com>
2021-10-29 19:54:10 -0500

    ejercicio calculadora

---
+++ Clase-29-10-2021-RepasoPOO/js/poo.js
@@ -0,0 +1,130 @@
+let container_calc = document.querySelector("#vista-botones")
+let btns_calc = ''
+for (let btn = 1; btn <= 15; btn++) {
+    if (btn <= 9) {
+        btns_calc += `
+        <div class="col-4 py-4 d-flex justify-content-center align-items-center">
+            <button class="btn btn-dark w-100" onclick="guardar_numero(${btn})">${btn}</button>
+        </div>
+        `
+    }
+    if (btn == 10) {
+        btns_calc += `
+        <div class="col-6 py-4 d-flex justify-content-center align-items-center">
+            <button class="btn btn-dark w-100" onclick="guardar_numero(0)">0</button>
+        </div>
+        `
+    }
+    switch (btn) {
+        case 11:
+            btns_calc += `
+            <div class="col-1 py-4 d-flex justify-content-center align-items-center">
+                <button class="btn btn-dark w-100" onclick="guardar_numero('', true)">=</button>
+            </div>
+            `
+            break;
+        case 12:
+            btns_calc += `
+            <div class="col-1 py-4 d-flex justify-content-center align-items-center">
+                <button class="btn btn-dark w-100" onclick="guardar_numero('suma', true)">+</button>
+            </div>
+            `
+            break;
+        case 13:
+            btns_calc += `
+            <div class="col-1 py-4 d-flex justify-content-center align-items-center">
+                <button class="btn btn-dark w-100" onclick="">-</button>
+            </div>
+            `
+            break;
+        case 14:
+            btns_calc += `
+            <div class="col-1 py-4 d-flex justify-content-center align-items-center">
+                <button class="btn btn-dark w-100" onclick="">x</button>
+            </div>
+            `
+            break;
+        case 15:
+            btns_calc += `
+            <div class="col-1 py-4 d-flex justify-content-center align-items-center">
+                <button class="btn btn-dark w-100" onclick="">/</button>
+            </div>
+            `
+            break;
+    }
+}
+container_calc.innerHTML = btns_calc
+
+class calculadora_operaciones {
+
+    numero1 = 0
+    numero2 = 0
+    resultado = 0
+
+    constructor(num1, num2) {
+        this.numero1 = num1
+        this.numero2 = num2
+    }
+
+    suma() {
+        this.resultado = parseInt(this.numero1) + parseInt(this.numero2)
+        return this.resultado
+    }
+
+    resta() {
+        this.resultado = parseInt(this.numero1) - parseInt(this.numero2)
+        return this.resultado
+    }
+
+    multiplicacion() {
+        this.resultado = parseInt(this.numero1) * parseInt(this.numero2)
+        return this.resultado
+    }
+
+    division() {
+        this.resultado = parseInt(this.numero1) / parseInt(this.numero2)
+        return this.resultado
+    }
+
+
+}
+
+// let operando = new calculadora_operaciones(5, 2)
+// console.log(operando.suma())
+// console.log(operando.resta())
+// console.log(operando.multiplicacion())
+// console.log(operando.division())
+
+function guardar_numero(numero_boton, accion_localstorage = false) {
+    var input_calculadora = document.querySelector("#pantalla_calculadora")
+    if (!accion_localstorage) {
+        input_calculadora.value += numero_boton
+    } else {
+        if (localStorage.getItem("primer_valor_declarado") == null) {
+            // localStorage.clear()
+            localStorage.setItem("numero1", input_calculadora.value)
+            localStorage.setItem("operacion", numero_boton)
+            input_calculadora.value = ''
+            localStorage.setItem("primer_valor_declarado", true)
+        } else {
+            resultado(input_calculadora.value)
+                // localStorage.setItem("numero2", input_calculadora.value)
+        }
+    }
+}
+
+function resultado(numero_siguiente) {
+    console.log(numero_siguiente)
+    var input_calculadora = document.querySelector("#pantalla_calculadora")
+    let num1 = localStorage.getItem("numero1")
+    let operacion = localStorage.getItem("operacion")
+    console.log(operacion)
+
+    let operando = new calculadora_operaciones(num1, numero_siguiente)
+
+    switch (operacion) {
+        case "suma":
+            input_calculadora.value = operando.suma()
+            break
+    }
+}
