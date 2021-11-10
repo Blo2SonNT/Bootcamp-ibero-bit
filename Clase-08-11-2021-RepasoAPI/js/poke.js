816a446b3b418c50bff52e09f47ca639b63e6238
Miguel Nieto <migustanieto.2@gmail.com>
2021-11-10 18:49:03 -0500

    Convirtiendo JSON en array - Accediendo a posicion especifica

--- Clase-08-11-2021-RepasoAPI/js/poke.js
+++ Clase-08-11-2021-RepasoAPI/js/poke.js
@@ -7,9 +7,21 @@ info_api.then(respuesta_api => respuesta_api.json())
         console.log(poke_info.sprites.other.dream_world.front_default)
         let info = poke_info.sprites.other['official-artwork']
         console.log(info.front_default)
-            // let imagen = Object.values(info)
-            // console.log(imagen[2])
-        document.getElementById("imagen").innerHTML = `
+        document.getElementById("imagen1").innerHTML = `
         <img src="${info.front_default}" alt="">
+        `
+    })
+
+
+// codigo convirtiendo el json en array
+let url_pokemon2 = "https://pokeapi.co/api/v2/pokemon/ditto"
+let info_api2 = fetch(url_pokemon2)
+
+info_api2.then(respuesta_api2 => respuesta_api2.json())
+    .then(poke_info2 => {
+        // console.log(poke_info2.sprites.other.dream_world.front_default)
+        let info2 = poke_info2.sprites.other['official-artwork']
+        document.getElementById("imagen2").innerHTML = `
+        <img src="${info2.front_default}" alt="">
         `
     })
