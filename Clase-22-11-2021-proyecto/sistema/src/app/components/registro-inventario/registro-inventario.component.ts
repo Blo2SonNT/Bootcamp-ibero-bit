1bea6d00c0eea16107a7c927dd1ff18995e110b0
Miguel Nieto <migustanieto.2@gmail.com>
2021-11-23 21:03:27 -0500

    Formularios reactivos

--- Clase-22-11-2021-proyecto/sistema/src/app/components/registro-inventario/registro-inventario.component.ts
+++ Clase-22-11-2021-proyecto/sistema/src/app/components/registro-inventario/registro-inventario.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
-  selector: 'app-registro-inventario',
-  templateUrl: './registro-inventario.component.html',
-  styleUrls: ['./registro-inventario.component.css']
+	selector: 'app-registro-inventario',
+	templateUrl: './registro-inventario.component.html',
+	styleUrls: ['./registro-inventario.component.css']
 })
 export class RegistroInventarioComponent implements OnInit {
 
-  constructor() { }
+	inventarioForm: FormGroup;
+	valorNumerico = /^[0-9]+$/;
 
-  ngOnInit(): void {
-  }
+	constructor(private fb: FormBuilder) {
+		this.inventarioForm = this.fb.group({
+			nombreProducto: ['', Validators.required],
+			fecVenceProducto: ['', Validators.required],
+			precioProducto: ['', [Validators.required, Validators.pattern(this.valorNumerico)]],
+			cantidadProducto: ['', [Validators.required, Validators.pattern(this.valorNumerico)]],
+			proveedorProducto: ['', Validators.required]
+		})
+	}
+
+	ngOnInit(): void {
+	}
+
+	guardarInventario(){
+		console.log(this.inventarioForm);
+	}
 
 }
