ea25d64c58babb8a1336baf718b2d5622c61a2f0
Miguel Nieto <migustanieto.2@gmail.com>
2021-12-13 19:20:09 -0500

    renderer 2 - modificacion del DOM - ng build

--- Clase-22-11-2021-proyecto/sistema/src/app/components/inicio/inicio.component.ts
+++ Clase-22-11-2021-proyecto/sistema/src/app/components/inicio/inicio.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-inicio',
@@ -7,9 +7,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class InicioComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild('cuadro') cuadroHTML?: ElementRef;
+  @ViewChild('natilla') natillaHTML?: ElementRef;
+
+  constructor( private renderer2: Renderer2 ) { }
 
   ngOnInit(): void {
+    console.log("Hola papa noel")
+  }
+
+  cambio_cuadro(){
+    const elementoHTML = this.cuadroHTML?.nativeElement;
+    console.log(elementoHTML);
+    // elementoHTML.style.border = 'solid 2px red';
+    this.renderer2.setStyle(elementoHTML, 'border', 'solid 2px red');
+    this.renderer2.setStyle(elementoHTML, 'background-color', '#26ff09');
+  }
+
+  cambio_imagen(){
+    const navidadHTML = this.natillaHTML?.nativeElement;
+    this.renderer2.setAttribute(navidadHTML, 'src', 'https://mandolina.co/wp-content/uploads/2020/11/natilla-con-salsa-de-mora.jpg');
   }
 
 }
